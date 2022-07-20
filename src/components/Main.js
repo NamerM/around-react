954f1e1896ec5fdbc664ffc8d5d016450d5899e8
Melih Namer <mnamer12@outlook.com>
2022-07-20 15:00:25 +0300

    Feature: Avatar Popup trial

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,29 +1,77 @@
 import React from 'react';
 import profileImage from '../images/image.jpg';
 import profileButton from '../images/edit-pen.svg';
-
+import addButton from '../images/add-button.svg';
 
 function Main() {
-return (
-  <main className="content">
-    <section className="profile">
-    <div className="profile__image"> <img className="profile__imagecontent" src={profileImage} alt="Profile User" /> </div>
 
-      <div className="profile__info">
-        <div className="profile__line-header">
-          <h2 className="profile__header">Cousteau </h2>
-          <button type="button" className="profile__button"><img src={profileButton} className="profile__button_image" alt="Button" /></button>
+
+  function togglePopup(){
+    document.body.classList.toggle('popup_open', isActive);
+  }
+
+  function PopupAvatar(){
+    const [isActive, setIsActive] = React.useState(false);
+
+      React.useEffect( () => {
+        togglePopup(isActive)
+        }
+      );
+
+      function handleEditAvatarClick(){
+        setIsActive(!isActive);
+      }
+      return (
+        <section className="popup popup_type_avatar-change" id="popup-template-avatar" onClick={handleEditAvatarClick}>
+          <div className="popup__square">
+            <form name="avatarChange" className="popup__form popup__form_avatar" novalidate>
+              <h1 className="popup__title">Change Profile Picture</h1>
+              <label className="popup__formfield">
+                <input className="popup__input popup__input_type_link" type="url" placeholder="Picture Url"  id="link" name="link"  required />
+                <span id="link-error" className="popup__input-error"></span>
+              </label>
+              <button type='submit' className="popup__save" >Save</button>
+              <button type='button' className='popup__close popup__close_add'></button>
+            </form>
+          </div>
+        </section>
+          );
+    }
+
+
+
+
+  // function handleEditProfile(){
+  //   setIsActive(true);
+  // }
+
+  // function handleAddPlaceClick(){
+  //   setIsActive(true);
+  // }
+
+
+
+return (
+  <main classNameName="content">
+    <section classNameName="profile">
+    <div classNameName="profile__image"> <img classNameName="profile__imagecontent" src={profileImage} alt="Profile User" /> </div>
+
+      <div classNameName="profile__info">
+        <div classNameName="profile__line-header">
+          <h2 classNameName="profile__header">Cousteau </h2>
+          <button type="button" classNameName="profile__button"><img src={profileButton} classNameName="profile__button_image" alt="Button" /></button>
         </div>
-        <p className="profile__profession">Explorer</p>
+        <p classNameName="profile__profession">Explorer</p>
       </div>
 
-      <button  type="button" className="add-button"></button>
+      <button  type="button" classNameName="add-button"><img src={addButton} classNameName="add-button__image" alt="Add Cards Button"></img></button>
     </section>
 
-    <section className="elements page__section">
-      <ul className="elements__cards"></ul>
+    <section classNameName="elements page__section">
+      <ul classNameName="elements__cards"></ul>
     </section>
   </main>
+
   )
 }
 
